81a33dc9c5d9a9fdafaa0dcebdef630a0a2055dd
mgrecu35 <mirgrecu@gmail.com>
2025-05-05 17:54:00 -0400

    precip alignment

---
+++ src_f90/fortran_alignment.f90
@@ -0,0 +1,96 @@
+!python -m numpy.f2py -c -m precip_align fortran_alignment.f90 
+
+subroutine calc_correlation(array1, array2, nx, ny, corr)
+    real, intent(in) :: array1(nx,ny), array2(nx,ny)
+    real, intent(out) :: corr
+    real :: array1_mean, array2_mean
+    real :: array1_std, array2_std
+    integer :: nx, ny
+    integer :: i, j
+    ! Compute correlation coefficient (placeholder for actual implementation)
+    corr = 0.0
+    !nx=size(array1, 1)
+    !ny=size(array1, 2)
+    array1_mean = sum(array1) / (nx * ny)
+    array2_mean = sum(array2) / (nx * ny)
+    array1_std = sqrt(sum((array1 - array1_mean)**2) / (nx * ny))+1e-9
+    array2_std = sqrt(sum((array2 - array2_mean)**2) / (nx * ny))+1e-9
+    do i=1,nx
+        do j=1,ny
+            corr = corr + (array1(i,j) - array1_mean) * (array2(i,j) - array2_mean)
+        end do
+    end do
+    corr = corr / (nx * ny * array1_std * array2_std)
+end subroutine calc_correlation
+
+subroutine gaussian_filter(input, output, n, sigma)
+    integer, intent(in) :: input(n)
+    real, intent(out) :: output(n)
+    integer, intent(in) :: sigma
+    integer :: i, j, n
+    real :: weight, weight_sum
+    ! Apply Gaussian filter (placeholder for actual implementation)
+    output = input
+    do i=1+sigma, n-sigma
+        weight_sum = 0.0
+        do j=-sigma, sigma
+            weight = exp(-j**2 / (2.0 * sigma**2))
+            output(i) = output(i) + input(i+j) * weight
+            weight_sum = weight_sum + weight
+        end do
+        output(i) = output(i) / weight_sum
+    end do
+end subroutine gaussian_filter
+
+subroutine align_precip(precip_gmi_only, precip_corra, precip_gmi_only_shifted, n_size, n_ray)
+    implicit none
+    integer :: nw2, nmax, n_size, i, j, index, n_ray
+    real :: precip_gmi_only(n_size,n_ray), precip_corra(n_size,n_ray)
+    real, allocatable :: corr(:)
+    integer, allocatable :: displacement(:)
+    real, allocatable :: inv_displacement(:)
+    real, intent(out) :: precip_gmi_only_shifted(n_size,n_ray)
+    integer :: sigma
+    integer :: max_index
+    !Initialize variables
+    nw2 = 4
+    nmax = 4
+
+    allocate(displacement(n_size))
+    allocate(inv_displacement(n_size))
+    !allocate(act_corr(0))
+    !allocate(index_L(0, 2))
+    allocate(corr(2 * nmax + 1))
+
+! Main loop
+    do i = nw2 + nmax+1, n_size - nw2 - nmax
+        corr = 0.0
+        do j = -nmax, nmax
+            if (maxval(precip_gmi_only(i + j - nw2:i + j + nw2, :)) < 0.01 .or. &
+                    maxval(precip_corra(i - nw2:i + nw2, :)) < 0.01) then
+                corr(j + nmax + 1) = 0.0
+            else
+                call calc_correlation(precip_gmi_only(i + j - nw2:i + j + nw2, :), &
+                    precip_corra(i - nw2:i + nw2, :),2*nw2+1,n_ray,corr(j + nmax + 1))
+            end if
+        end do
+        max_index = maxloc(corr, dim=1)
+        index = max_index - 1
+        displacement(i) = index - 4
+    end do
+    deallocate(corr)
+
+
+! Apply Gaussian filter (placeholder for actual implementation)
+    sigma = 1
+    call gaussian_filter(displacement, inv_displacement, n_size, sigma)
+
+! Shift precip_gmi_only
+    do i = 1, size(precip_gmi_only, 1)
+        do j = 1, size(precip_gmi_only, 2)
+            if (i + inv_displacement(i) > 0 .and. i + inv_displacement(i) <= size(precip_gmi_only, 1)) then
+                precip_gmi_only_shifted(i, j) = precip_gmi_only(int(i + inv_displacement(i)), j)
+            end if
+        end do
+    end do
+end subroutine align_precip
--- test_gmi_only_gpmdev.py
+++ test_gmi_only_gpmdev.py
@@ -17,10 +17,12 @@ with nc.Dataset(fcmb[0]) as f:
     lat=f['KuKaGMI/Latitude'][:]
     lon=f['KuKaGMI/Longitude'][:]
     surf_type=f['KuKaGMI/Input/surfaceType'][:]
+    srf_bin=f['KuKaGMI/Input/surfaceRangeBin'][:,:,0]
     qv=f['KuKaGMI/vaporDensity'][:]
     sk_temp=f['KuKaGMI/skinTemperature'][:]
     oe_wvp=f['KuKaGMI/OptEst/OEcolumnWaterVapor'][:]
     near_sfc_precip=f['KuKaGMI/nearSurfPrecipTotRate'][:]
+    pRate=f['KuKaGMI/precipTotRate'][:]
 
 print(lon.shape)
 import numpy as np
@@ -55,21 +57,75 @@ print(sk_temp[n1:n2].T.shape)
 print(qv[n1:n2].T.shape)
 
 x_output_f, xoutput_profs = onnx_f90.test_model_1d_onnx(tb_resampled[n1:n2],surf_type[n1:n2].T,sk_temp[n1:n2].T,qv[n1:n2].T,nfeat_in,nfeat_out)
-
+for i in range(n1,n2):
+    for j in range(49):
+        if surf_type[i,j] == 0:
+            xoutput_profs[i-n1,j,84:srf_bin[i,j]]=xoutput_profs[i-n1,j,83]
+        else:
+            xoutput_profs[i-n1,j,82:srf_bin[i,j]]=xoutput_profs[i-n1,j,81]
 import matplotlib.pyplot as plt
 import matplotlib
 import cartopy.crs as ccrs
 
-plt.figure(figsize=(10,10))
+n_scans=near_sfc_precip.shape[0]
+import precip_align as pal
+
+#w1=np.random.randn(9,49)
+#w2=0.6*w1+0.4*np.random.randn(9,49)
+#corr_coeff=np.corrcoef(w1.flatten(),w2.flatten())[0,1]
+#corr_coeff_f90=pal.calc_correlation(w1,w2)
+#print(corr_coeff,corr_coeff_f90)
+#precip_gmi_only_shifted = pal.align_precip(precip_gmi_only,precip_corra)
+for i in range(0,n_scans,300):
+    n1i=i
+    n2i=i+300
+    print(n1i,n2i)
+    x_output_fi, xoutput_profsi = onnx_f90.test_model_1d_onnx(tb_resampled[n1i:n2i],surf_type[n1i:n2i].T,sk_temp[n1i:n2i].T,qv[n1i:n2i].T,nfeat_in,nfeat_out)
+    precip_corra=near_sfc_precip[n1i:n2i,:]
+    precip_gmi_only=x_output_fi[1,:,:].T
+    precip_gmi_only_shifted = pal.align_precip(precip_gmi_only,precip_corra)
+    cc1=np.corrcoef(near_sfc_precip[n1i:n2i,:].flatten(),x_output_fi[1,:,:].T.flatten())
+    cc2=np.corrcoef(precip_gmi_only_shifted.flatten(),x_output_fi[1,:,:].T.flatten())
+    print(cc1[0,1],cc2[0,1])
+stop
+plt.figure(figsize=(8,6))
 n1=4800
 n2=5100
-ax=plt.subplot(1,1,1,projection=ccrs.PlateCarree())
-x_output_f[1,:,:][x_output_f[1,:,:]<0.1]=0.0
+ax=plt.subplot(1,2,1,projection=ccrs.PlateCarree())
+# plot states and coastlines
+x_output_f[1,:,:][x_output_f[1,:,:]<0.05]=0.0
 plt.pcolormesh(lon[n1:n2,:],lat[n1:n2,:],x_output_f[1,:,:].T,cmap='jet',norm=matplotlib.colors.LogNorm(vmin=0.1,vmax=100))
 ax.coastlines()
+import cartopy.feature as cfeature
 
-plt.figure()
-xoutput_profs[xoutput_profs<0.1]=0.0
+# Add US states to the plot
+ax.add_feature(cfeature.STATES, edgecolor='black', linewidth=0.5)
+plt.title("GMI-only surface precipitation")
+plt.colorbar(orientation='horizontal',pad=0.05,shrink=0.8)
+ax=plt.subplot(1,2,2,projection=ccrs.PlateCarree())
+plt.pcolormesh(lon[n1:n2,:],lat[n1:n2,:],near_sfc_precip[n1:n2,:] ,cmap='jet',norm=matplotlib.colors.LogNorm(vmin=0.1,vmax=100))
+ax.add_feature(cfeature.STATES, edgecolor='black', linewidth=0.5)
+ax.coastlines()
+plt.title("CORRA V07 surface precipitation")
+plt.colorbar(orientation='horizontal',pad=0.05,shrink=0.8)
+plt.tight_layout()
+plt.savefig("GMI_only_precip_surface_map.png",dpi=300)
+
+plt.figure(figsize=(8,6))
+ax1=plt.subplot(2,1,1)
+xoutput_profs[xoutput_profs<0.05]=0.0
 plt.pcolormesh(lon[n1:n2,24],range(88),xoutput_profs[:,24,:].T,cmap='jet',norm=matplotlib.colors.LogNorm(vmin=0.1,vmax=100))
 plt.ylim(87,20)
+plt.ylabel("Range bin")
+ax1.xaxis.set_visible(False)
 plt.colorbar()
+plt.title("GMI-only precipitation")
+plt.subplot(2,1,2)
+plt.pcolormesh(lon[n1:n2,24],range(88),pRate[n1:n2,24,:].T,cmap='jet',norm=matplotlib.colors.LogNorm(vmin=0.1,vmax=100))
+plt.ylim(87,20)
+plt.xlabel("Longitude")
+plt.ylabel("Range bin")
+plt.colorbar()
+plt.title("CORRA V07 precipitation")
+plt.tight_layout()
+plt.savefig("GMI_only_precip_cross_section.png",dpi=300)
